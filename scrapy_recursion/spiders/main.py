c255d9dbee14422e67291cf7980fa9865eac3019
nana89823 <nana89823@gmail.com>
2024-01-05 13:41:23 +0800

    Add start url

--- scrapy_recursion/spiders/main.py
+++ scrapy_recursion/spiders/main.py
@@ -3,19 +3,38 @@ from scrapy.spiders import CrawlSpider, Rule
 from .. import items
 import logging
 
-logger = logging.getLogger('mycustomlogger')  # 用python 的logging,這行為log取名
+logger = logging.getLogger("mycustomlogger")  # 用python 的logging,這行為log取名
+
 
 class MySpider(CrawlSpider):
     name = "recurison"
-    allowed_domains = ["news.pts.org.tw", "www.don1don.com", "www.everydayobject.us", "www.ettoday.net", "www.dramaqueen.com.tw", "applianceinsight.com.tw", "easylife.tw", "e-creative.media", "ahui3c.com", "anntw.com", "www.ltn.com.tw"]
-    custom_settings = {
-        'DOWNLOADER_MIDDLEWARES':{'scrapy_recursion.middlewares.PerStartUrlTimeoutMiddleware': 543},
-        'ITEM_PIPELINES': {"scrapy_recursion.pipelines.LinksPipeline": 500},
-        'START_URL_TIMEOUT': 3600, 
-    }
-    rules = [
-        Rule(LinkExtractor(), callback="parse_page", follow=True)
+    allowed_domains = [
+        "news.pts.org.tw",
+        "www.don1don.com",
+        "www.everydayobject.us",
+        "www.ettoday.net",
+        "www.dramaqueen.com.tw",
+        "applianceinsight.com.tw",
+        "easylife.tw",
+        "e-creative.media",
+        "ahui3c.com",
+        "anntw.com",
+        "www.ltn.com.tw",
+        "www.businesstoday.com.tw",
+        "www.businessweekly.com.tw",
+        "csr.cw.com.tw",
+        "news.cts.com.tw",
+        "www.msn.com",
+        "www.mirrormedia.mg"
     ]
+    custom_settings = {
+        "DOWNLOADER_MIDDLEWARES": {
+            "scrapy_recursion.middlewares.PerStartUrlTimeoutMiddleware": 543
+        },
+        "ITEM_PIPELINES": {"scrapy_recursion.pipelines.LinksPipeline": 500},
+        "START_URL_TIMEOUT": 3600,
+    }
+    rules = [Rule(LinkExtractor(), callback="parse_page", follow=True)]
     count = 0
 
     def __init__(self, *args, **kwargs):
